15327abb9b2c5ca62de02ae027a5aca675ff0585
mari7102 <55129539+mari7102@users.noreply.github.com>
2019-09-23 11:04:46 +0200

    Opgave slut

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  //https://swapi.co/api/people/
+
+  state = {
+    personer: []
+  }
+
+  //Hent data når component er mount/klar
+  componentDidMount() {
+
+    //brug axios til at kontakte + hente data fra api
+    axios.get('https://swapi.co/api/people/').then(resultat => {
+      //console.log(resultat.data.results);
+
+      this.setState({
+        personer: resultat.data.results
+      });
+
+    });
+  }
+
+  render() {
+
+    const allepersoner = this.state.personer;
+
+    //map'er (kortlægger) data - hvordan og hvad der skal vises
+    const personListe = allepersoner.map((person, index) => {
+
+      return (
+        <div key={index}>
+          <b>Navn:</b> {person.name} <b>Højde:</b> {person.height} <b>Født:</b> {person.birth_year}
+        </div>
+      )
+    });
+
+    return (
+      <div>
+
+        <h1>Personer</h1>
+        {personListe}
+
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
